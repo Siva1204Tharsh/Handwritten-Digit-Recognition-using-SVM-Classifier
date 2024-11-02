3f2cc01ec592881579ad37f33c57081788b61414
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-11-02 10:01:41 +0530

    k

--- handwrittendigitrecognition_svm.py
+++ handwrittendigitrecognition_svm.py
@@ -13,7 +13,7 @@ dataiamgeLength=len(dataset.images)
 # visualize the dataset
 
 # n=9 #no of sample out   of samples total 1797
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 # plt.gray()
 # plt.matshow(dataset.images[n])
 # plt.show()
@@ -44,3 +44,61 @@ Y_pred = model_svm.predict(X_test)
 #Evaluation - Accuracy
 from sklearn.metrics import accuracy_score
 print("Accuracy of the model:{0}%".format(accuracy_score(Y_test, Y_pred)*100))
+
+
+#Prediction
+n=15
+result = model_svm.predict(dataset.images[n].reshape((1,-1)))
+plt.imshow(dataset.images[n], cmap=plt.cm.gray_r, interpolation='nearest')     
+print(result)
+print("\n")
+plt.axis('off')
+plt.title('%i' %result)
+plt.show()
+
+
+
+
+
+
+#playing kernal with different values
+# model1=SVC(kernel='linear')
+# model2=SVC(kernel='rbf')
+# model3=SVC(kernel='poly')
+# model4=SVC(kernel='sigmoid')
+# model5=SVC()
+# model6=SVC(gamma=0.001)
+# model7=SVC(gamma=0.001 ,C=0.1)
+
+# model1.fit(X_train, Y_train)
+# model2.fit(X_train, Y_train)
+# model3.fit(X_train, Y_train)
+# model4.fit(X_train, Y_train)
+# model5.fit(X_train, Y_train)
+# model6.fit(X_train, Y_train)
+# model7.fit(X_train, Y_train)
+
+# Y_pred1 = model1.predict(X_test)
+# Y_pred2 = model2.predict(X_test)
+# Y_pred3 = model3.predict(X_test)
+# Y_pred4 = model4.predict(X_test)
+# Y_pred5 = model5.predict(X_test)
+# Y_pred6 = model6.predict(X_test)
+# Y_pred7 = model7.predict(X_test)
+
+# print("Accuracy of the model1:{0}%".format(accuracy_score(Y_test, Y_pred1)*100))
+# print("Accuracy of the model2:{0}%".format(accuracy_score(Y_test, Y_pred2)*100))
+# print("Accuracy of the model3:{0}%".format(accuracy_score(Y_test, Y_pred3)*100))
+# print("Accuracy of the model4:{0}%".format(accuracy_score(Y_test, Y_pred4)*100))
+# print("Accuracy of the model5:{0}%".format(accuracy_score(Y_test, Y_pred5)*100))
+# print("Accuracy of the model6:{0}%".format(accuracy_score(Y_test, Y_pred6)*100))
+# print("Accuracy of the model7:{0}%".format(accuracy_score(Y_test, Y_pred7)*100)) 
+
+# Accuracy of the model:97.77777777777777%
+# Accuracy of the model1:97.77777777777777%
+# Accuracy of the model2:99.16666666666667%
+# Accuracy of the model3:98.88888888888889%
+# Accuracy of the model4:91.38888888888889%
+# Accuracy of the model5:99.16666666666667%
+# Accuracy of the model6:99.16666666666667%
+# Accuracy of the model7:97.22222222222221%
