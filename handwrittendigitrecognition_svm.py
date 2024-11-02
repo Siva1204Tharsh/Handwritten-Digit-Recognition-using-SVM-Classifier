88b63836d6ca1d8dd6c723e723dc0bd2837501ac
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-11-02 09:48:17 +0530

    l

--- handwrittendigitrecognition_svm.py
+++ handwrittendigitrecognition_svm.py
@@ -7,12 +7,40 @@ dataset = load_digits()
 # print(dataset.target)
 # print(dataset.data.shape)
 # print(dataset.target.shape)
+dataiamgeLength=len(dataset.images)
+# print(dataiamgeLength)
 
 # visualize the dataset
 
-n=9 #no of sample out   of samples total 1797
-import matplotlib.pyplot as plt
-plt.gray()
-plt.matshow(dataset.images[n])
-plt.show()
-dataset.iamges[n]
+# n=9 #no of sample out   of samples total 1797
+# import matplotlib.pyplot as plt
+# plt.gray()
+# plt.matshow(dataset.images[n])
+# plt.show()
+# dataset.iamges[n]
+
+#Segregate the Y and X
+X=dataset.images.reshape((dataiamgeLength,-1))
+# print(X)
+Y=dataset.target
+# print(Y)
+
+#Split the dataset into training and testing
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+# print(X_train.shape)
+# print(X_test.shape)
+
+#Trainng the SVM
+from sklearn.svm import SVC
+model_svm = SVC(kernel='linear')
+model_svm.fit(X_train, Y_train)
+
+#Predict the testing data
+Y_pred = model_svm.predict(X_test)
+# print(Y_pred)
+# print(np.concatenate(( Y_pred.reshape(len(Y_pred),1), Y_test.reshape(len(Y_test),1)), 1))
+
+#Evaluation - Accuracy
+from sklearn.metrics import accuracy_score
+print("Accuracy of the model:{0}%".format(accuracy_score(Y_test, Y_pred)*100))
